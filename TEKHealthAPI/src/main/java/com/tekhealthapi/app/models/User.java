c5fad3c2590577abe58c65b1c352daed0c76f50c
Madishetti <pmadishe@teksystems.com>
2018-08-01 17:10:52 +0530

    Signed-off-by: Madishetti <pmadishe@teksystems.com>

---
+++ TEKHealthAPI/src/main/java/com/tekhealthapi/app/models/User.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package com.tekhealthapi.app.models;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * @author Pramod M
+ *
+ */
+@Document
+public class User{
+
+	@Id
+	String userId;
+	String firstName;
+	String lastName;
+	String emailId;
+	String password;
+        String mobileNumber;
+        String country;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+        
+
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+	
+}
