d98adb9403acd3f6d5c3af7983d1d17515bfc3d5
balucse535 <Always2R@ma>
2018-09-25 01:38:00 +0530

    Code commit

--- TEKHealthAPI/src/main/java/com/tekhealthapi/app/controller/UserDeviceDataController.java
+++ TEKHealthAPI/src/main/java/com/tekhealthapi/app/controller/UserDeviceDataController.java
@@ -5,26 +5,41 @@
  */
 package com.tekhealthapi.app.controller;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
 import com.tekhealthapi.app.models.UserDeviceDataRepository;
 import com.tekhealthapi.app.models.UserDeviceData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 /**
  *
  * @author Pramod M
  */
+
+
+
 @RestController
 @RequestMapping(value = "/devicedata")
 public class UserDeviceDataController {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
 
     private final Logger LOG = LoggerFactory.getLogger(getClass());
 
@@ -55,16 +70,28 @@ public class UserDeviceDataController {
         LOG.info("Getting userDeviceData with patientId: {}.", patientId);
         LOG.info("Getting userDeviceData with fromDate: {}.", fromDate);
         LOG.info("Getting userDeviceData with toDate: {}.", toDate);
-        Date frDate=new Date(),tooDate=new Date();
+        Date frDate=new Date();
+        Date tooDate=new Date();
+        SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        dtFormat.setTimeZone(TimeZone.getTimeZone("Asia/Calcutta"));
         try{
-             frDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(fromDate);
-             tooDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(toDate);
+             frDate = dtFormat.parse(fromDate);
+             tooDate = dtFormat.parse(toDate);
         }catch(ParseException e){
             e.printStackTrace();
         }
 
+        Query query=new Query(
+                Criteria.where("patientId").regex(patientId, "i")
+                .andOperator(
+                        Criteria.where("createdTimestamp").lt(tooDate),
+                        Criteria.where("createdTimestamp").gte(frDate)
+                )
 
-        return userDeviceDataRepository.findByPatientIdAndCreatedTimestampBetween(patientId, frDate, tooDate);
+        );
+        System.out.println("Query>>>>>>"+query.toString());
+        return mongoTemplate.find(query, UserDeviceData.class);
+
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
--- TEKHealthAPI/src/main/java/com/tekhealthapi/app/models/UserDeviceDataRepository.java
+++ TEKHealthAPI/src/main/java/com/tekhealthapi/app/models/UserDeviceDataRepository.java
@@ -2,6 +2,7 @@ package com.tekhealthapi.app.models;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,8 +10,8 @@ import java.util.Date;
 import java.util.List;
 
 @Transactional
-public interface UserDeviceDataRepository extends Repository<UserDeviceData, String>, MongoRepository<UserDeviceData,String> {
+public interface UserDeviceDataRepository extends MongoRepository<UserDeviceData,String> {
 
-    @Query("{\"patientId\":?0,\"createdTimestamp\":{\"$lte\":?1,\"$gte\":?2}}")
-    List<UserDeviceData> findByPatientIdAndCreatedTimestampBetween(String patientId,Date fromDate,Date toDate);
+//    @Query("{\"patientId\":?0,\"createdTimestamp\":{\"$lte\":?1,\"$gte\":?2}}")
+//    List<UserDeviceData> findByPatientIdAndCreatedTimestampBetween(String patientId,Date fromDate,Date toDate);
 }
